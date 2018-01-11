02e93a6b7b41e952411b3c0324a6a7410b7aa371
clafleur <clafleur@users.noreply.github.com>
2018-01-11 15:07:33 -0800

    added movement logic
    Added player movement logic and clamping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float speed = 5.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        transform.position = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        //horizontal Movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+        //vertical Movement
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        //clamp movement of player on the y axis
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        //clamp movement of player on the x axis
+        if (transform.position.x > 8.2f)
+        {
+            transform.position = new Vector3(8.2f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -8.2f)
+        {
+            transform.position = new Vector3(-8.2f, transform.position.y, 0);
+        }
+    }
 }
