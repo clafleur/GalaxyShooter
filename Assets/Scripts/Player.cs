fc77d08bfd5e19435706905085e21697eac18ad4
clafleur <clafleur@users.noreply.github.com>
2018-01-12 09:36:46 -0800

    Implemented Player movement
    Finished PlayerMovement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 5.0f;
+    [SerializeField]
+    private float speed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
